ad46cf5db93fe412ce2e860b0ea655e0cc038905
shenmiao <shenmiao@zjaisino.com>
2017-06-23 10:25:21 +0800

    第一次导入

---
+++ src/main/java/com/sm/hello/controller/SampleController.java
@@ -0,0 +1,10 @@
+package com.sm.hello.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+	
+}
